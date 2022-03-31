ba0b9d05d13cff94c3a079d0fedf0c7c1e6d8201
atharva20038 <atharva20038@iiitd.ac.in>
2022-03-31 16:57:15 +0530

    typescript_commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Header title="Hello World" color="red"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
